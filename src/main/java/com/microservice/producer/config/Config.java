bc0b285d2c80b050bcc8d423fea588185974daa0
nugriz <rizki@happy5.co>
2023-07-04 17:11:57 +0700

    specify props & config for kafka

--- src/main/java/com/microservice/producer/config/Config.java
+++ src/main/java/com/microservice/producer/config/Config.java
@@ -1,46 +1,33 @@
 package com.microservice.producer.config;
 
-import java.util.Map;
-import org.springframework.kafka.core.*;
-
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class Config {
-
-    private final KafkaProperties kafkaProperties;
-
-    @Autowired
-    public Config(KafkaProperties kafkaProperties) {
-        this.kafkaProperties = kafkaProperties;
-    }
-
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        // get configs on application.properties/yml
-        Map<String, Object> properties = kafkaProperties.buildProducerProperties();
-        return new DefaultKafkaProducerFactory<>(properties);
-    }
+    
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+    
+    @Value(value = "${message.topic.name}")
+    private String topicName;
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
     }
-
+    
     @Bean
-    public NewTopic topic() {
-        return TopicBuilder
-                .name("t.temp.sensor")
-                .partitions(1)
-                .replicas(1)
-                .build();
+    public NewTopic topic1() {
+         return new NewTopic(topicName, 1, (short) 1);
     }
-
 }
--- src/main/java/com/microservice/producer/service/producer/Producer.java
+++ src/main/java/com/microservice/producer/service/producer/Producer.java
@@ -1,8 +1,12 @@
 package com.microservice.producer.service.producer;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,13 +14,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservice.producer.model.TempSensor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
 public class Producer {
 
-    @Value("${topic.name}")
-    private String orderTopic;
+    @Value("${message.topic.name}")
+    private String topicName;
 
     private final ObjectMapper objectMapper;
     private final KafkaTemplate<String, String> kafkaTemplate;
@@ -29,9 +38,9 @@ public class Producer {
 
     public String sendMessage(TempSensor tempSensor) throws JsonProcessingException {
         String orderAsMessage = objectMapper.writeValueAsString(tempSensor);
-        kafkaTemplate.send(orderTopic, orderAsMessage);
+        kafkaTemplate.send(topicName, orderAsMessage);
 
-        log.info("food order produced {}", orderAsMessage);
+        log.info("temperature sensor produced {}", orderAsMessage);
 
         return "message sent";
     }
